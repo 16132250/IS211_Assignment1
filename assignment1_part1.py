8ac88212cf6a91bfd4cecf7fb7182f2b61741c37
16132250 <jason.curate50@spsmail.cuny.edu>
2022-09-25 20:43:39 -0400

    Final Commit

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -1,26 +1,35 @@
 
+class ListDivideException(Exception):
+    pass
 def list_divide(numbers, divide=2):
     """
     The function returns the number of elements in the numbers list that are divisibleby divide
     """
-    divide_count = 0
-    for item in numbers:
-        if item % divide == 0:
-            divide_count += 1
-    # print(dividecount)
+    try:
+        divide_count = 0
+        for item in numbers:
+            if item % divide == 0:
+                divide_count += 1
+                # print(divide_count)
+        return divide_count
 
-    return divide_count
-
+    except ListDivideException:
+        print('there\'s a problem!')
 
 def test_list_divide():
     """
     Test listDivide
     """
     assert list_divide([1,2,3,4,5]) == 2
+    # print(f"1 Done!")
     assert list_divide([2,4,6,8,10]) == 5
+    # print("2 Done!")
     assert list_divide([30, 54, 63,98, 100], divide=10) == 2
+    # print("3 Done!")
     assert list_divide([]) == 0
+    # print("4 Done!")
     assert list_divide([1,2,3,4,5], 1) == 5
-    
+    # print("5 Done!")
+
 if __name__ == "__main__":
     test_list_divide()
--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,11 +1,19 @@
 
 class Book:
+
     def __init__(self, author, title):
-        pass
-    
+        self.author = author
+        self.title = title
+
+
     def display(self):
-        pass
+        print(f'{self.title}, written by {self.author}')
+
+
+oman = Book('John Steinbeck','Of Mice and Men')
+tkam = Book('Harper Lee','To Kill a Mockingbird')
 
 
 if __name__ == "__main__":
-    pass
+    Book.display(oman)
+    Book.display(tkam)
